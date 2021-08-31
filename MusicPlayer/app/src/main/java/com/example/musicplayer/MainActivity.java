161f92af7c4fe3f9cf0e79cc754285b6c48f8980
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2021-08-31 14:08:13 +0700

    Update Audio Resource

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button playButton = (Button)findViewById(R.id.play_button_id);
+        Button pauseButton = (Button)findViewById(R.id.pause_button_id);
+        mp = MediaPlayer.create(this, R.raw.test_audio);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.pause();
+            }
+        });
+    }
+}
